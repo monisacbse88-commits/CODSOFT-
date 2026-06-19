91c5dfefee1f3b85483eb48d323c7cb2c9af708a
monisacbse88-commits <monisa.cbse.88@gmail.com>
2026-06-19 16:17:11 +0530

    Add files via upload
    A simple command-line calculator developed in Python. The application performs basic arithmetic operations including addition, subtraction, multiplication, division, quotient, and remainder calculations. It also handles division-by-zero errors and allows users to perform multiple calculations in a single session

---
+++ Simple Calculator.py
@@ -0,0 +1,35 @@
+#Simple Calculator
+print("Simple Calculator")
+print("Choose an operation:\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Quotient\n6. Remainder")
+while True:
+    num1=int(input("Enter number:"))
+    num2=int(input("Enter number:"))
+    choice=int(input("Enter choice:"))
+    if choice==1:
+        res=num1+num2
+    elif choice==2:
+        if num2>num1:
+            num1,num2=num2,num1
+        res=num1-num2
+    elif choice==3:
+        res=num1*num2
+    elif choice==4:
+        if num2==0:
+            print("Zero Division error has occured")
+            continue
+        res=num1/num2
+    elif choice==5:
+        if num2==0:
+            print("Zero Division error occured")
+            continue
+        res=num1//num2
+    elif choice==6:
+        if num2==0:
+            print("Zero division error occured")
+            continue
+        res=num1%num2
+    print("Result:",res)
+    c=input("Do you want to continue(y/n):")
+    if(c=='n'):
+        break
+print("All arithmetic operations are performed successfully!!")
